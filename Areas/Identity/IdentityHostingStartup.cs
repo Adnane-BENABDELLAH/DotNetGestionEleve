c73ce54e6fed34b08136e4739c9fbee4328dacb3
laidouniyoussef <laidoussef15@gmail.com>
2021-12-12 23:35:50 +0100

    Auth Added, Front Updated

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EnsatGestion.Areas.Identity.Data;
+using EnsatGestion.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EnsatGestion.Areas.Identity.IdentityHostingStartup))]
+namespace EnsatGestion.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<EnsatGestionContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("EnsatGestionContextConnection")));
+
+                services.AddDefaultIdentity<EnsatGestionUser>(options => {
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.SignIn.RequireConfirmedAccount = false;
+                })
+                    .AddEntityFrameworkStores<EnsatGestionContext>();
+            });
+        }
+    }
+}
